9f530b7ec8f78ee16ba3639ae41e8db76d8e4391
alexandr.anushko <shuratov2001@mail.ru>
2021-06-30 12:53:21 +0300

    set commands for user

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ require('dotenv').config()
 const TgBotAPI = require('node-telegram-bot-api')
 
 const tg_token = process.env.tg_token
+const stickers = require('./config.json')
 
 const bot = new TgBotAPI(tg_token, {polling: true})
+
+//set commands
+bot.setMyCommands([
+    {command: '/start', description: 'Приветсвие'},
+    {command: '/info', description: 'Информация'}
+])
 
 // start bot
 bot.on('message', msg => {
@@ -13,7 +20,12 @@ bot.on('message', msg => {
     const user_first_name = msg.chat.first_name
     // send response to user 
     if (user_text === '/start'){
+        bot.sendSticker(chat_id, stickers.stickers.deamon.welcome)
         bot.sendMessage(chat_id, `Добро пожаловать дорогой ${user_first_name}!`)
+    }
+    if (user_text === '/help'){
+        bot.sendSticker(chat_id, stickers.stickers.deamon.help)
+        bot.sendMessage(chat_id, 'Информация')
     }
     console.log(msg)
 })

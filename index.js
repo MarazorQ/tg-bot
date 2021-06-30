8a6c8beaea6fdb8e83f896b03ff1094ac54bcb29
alexandr.anushko <shuratov2001@mail.ru>
2021-06-30 12:11:50 +0300

    start bot

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 require('dotenv').config()
+
 const TgBotAPI = require('node-telegram-bot-api')
 
 const tg_token = process.env.tg_token
 
+const bot = new TgBotAPI(tg_token, {polling: true})
+
+bot.on('message', msg => {
+    const user_text = msg.text
+    const chat_id = msg.chat.id
+    console.log(`user message ${user_text}, chat id ${chat_id}`)
+})
